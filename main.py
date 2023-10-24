59fe2a4f6194ed0c6680bb52bc203ccc2a1827dd
sudeepmathew <sudeepmathew0@gmail.com>
2023-10-24 14:08:27 +0530

    Add More End Point

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[['STAID','STANAME                                 ']]
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 @app.route("/api/v1/<station>/<date>")
 def api(station, date):
@@ -17,6 +20,26 @@ def api(station, date):
         "date":date,
         "temperature":temperature
     }
+
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID"+ str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    result = df.to_dict(orient="records")
+    print(result)
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def all_year(station, year):
+    filename = "data_small/TG_STAID"+ str(station).zfill(6)+".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))]
+    result = result.to_dict(orient="records")
+    return result
+
 
 if __name__ == "__main__":
     app.run(debug=True)
