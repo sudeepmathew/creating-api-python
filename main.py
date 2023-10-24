4d15cc9297f0020634ed1ef35caa6bc0e63ab1df
sudeepmathew <sudeepmathew0@gmail.com>
2023-10-24 10:22:07 +0530

    API Initial Commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def api(station, date):
+    temperature = 20
+    return {
+        "station":station,
+        "date":date,
+        "temperature":temperature
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
